f0b7f241782e48a0fb2452afa14633b5f50d51ce
Sadra Taban <staban@flowpoint.co>
2022-05-04 11:30:41 -0400

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "main",
+  repositoryUrl: "https://github.com/sadrataban/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
